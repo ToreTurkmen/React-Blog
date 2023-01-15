c466523df862ce5bc759f842a61364883f475824
ToreTurkmen <=>
2023-01-15 23:49:58 +0100

    created pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-     Tore Turkmen
-    </div>
+   <BrowserRouter>
+   
+    <main>
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="about" element={ <About />} />
+        
+      </Routes>
+    </main>
+   
+   
+   </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <h2>Welcome</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ut id culpa error illum? Eveniet vel praesentium cupiditate aliquid, tempora odio nobis, numquam ab reprehenderit eum incidunt maxime tempore aliquam.
+      Debitis, rem quos, dignissimos magni, ut unde repudiandae nesciunt consectetur fugit veniam rerum beatae cumque modi optio? Doloremque eius non vitae harum vero neque quo at itaque! Est, veritatis eaque?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ut id culpa error illum? Eveniet vel praesentium cupiditate aliquid, tempora odio nobis, numquam ab reprehenderit eum incidunt maxime tempore aliquam.
+      Debitis, rem quos, dignissimos magni, ut unde repudiandae nesciunt consectetur fugit veniam rerum beatae cumque modi optio? Doloremque eius non vitae harum vero neque quo at itaque! Est, veritatis eaque?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ut id culpa error illum? Eveniet vel praesentium cupiditate aliquid, tempora odio nobis, numquam ab reprehenderit eum incidunt maxime tempore aliquam.
+      Debitis, rem quos, dignissimos magni, ut unde repudiandae nesciunt consectetur fugit veniam rerum beatae cumque modi optio? Doloremque eius non vitae harum vero neque quo at itaque! Est, veritatis eaque?</p>
+    </div>
+  )
+}
