e6ddf1b0a95ba887da48feec5d8e77f8da817884
ToreTurkmen <=>
2023-01-19 17:00:48 +0100

    created new authentication inside about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
+import { Navigate } from 'react-router-dom'
 
 export default function About () {
+
+  const [user, setUser] = useState('tore')
+
+  if(!user) {
+    return <Navigate to="/" replace={true}/>
+  }
   return (
     <div className='about'>
        <h2>About Us</h2>
@@ -10,6 +18,8 @@ export default function About () {
       Debitis, rem quos, dignissimos magni, ut unde repudiandae nesciunt consectetur fugit veniam rerum beatae cumque modi optio? Doloremque eius non vitae harum vero neque quo at itaque! Est, veritatis eaque?</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ut id culpa error illum? Eveniet vel praesentium cupiditate aliquid, tempora odio nobis, numquam ab reprehenderit eum incidunt maxime tempore aliquam.
       Debitis, rem quos, dignissimos magni, ut unde repudiandae nesciunt consectetur fugit veniam rerum beatae cumque modi optio? Doloremque eius non vitae harum vero neque quo at itaque! Est, veritatis eaque?</p>
+
+      <button onClick={() =>setUser(null)}>Logout</button>
     </div>
   )
 }
