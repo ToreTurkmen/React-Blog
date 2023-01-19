45ceb36712e3e8c13efc6727f675dd734a104d4a
ToreTurkmen <=>
2023-01-19 16:38:31 +0100

    added new form action prop

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import HelpLayout from "./layouts/HelpLayout";
 import About from "./pages/About";
 import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails";
 import Careers, { careersLoader } from "./pages/careers/Careers";
-import Contact from "./pages/help/Contact";
+import Contact, { contactAction } from "./pages/help/Contact";
 import Faq from "./pages/help/Faq";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -29,7 +29,7 @@ const router = createBrowserRouter(
       
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />}/>
-        <Route path="contact" element={<Contact />}/>
+        <Route path="contact" element={<Contact />} action={contactAction}/>
       </Route>
 
       <Route path="careers" element={<CareersLayout />} errorElement ={<CareersError />}>
--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,20 +1,47 @@
 import React from 'react'
+import { Form, redirect, useActionData } from 'react-router-dom'
 
 export default function Contact() {
+ 
+  const data = useActionData()
+
   return (
     <div className='contact'>
       <h3>Contact Us</h3>
-      <form>
+      <Form method='post' action='/help/contact'>
         <label>
           <span>Your mail:</span>
           <input type="email" name='email' required />
         </label>
         <label>
           <span>Your message:</span>
-          <textarea name='email' required> </textarea>
+          <textarea name='message' required> </textarea>
         </label>
         <button>Submit</button>
-      </form>
+        {data && data.error && <p>{data.error}</p>}
+      </Form>
     </div>
   )
+}
+
+export const contactAction = async ({request}) =>{
+  // console.log(request)
+
+  const data = await request.formData()
+
+  const submission = {
+    email: data.get('email'),
+    message: data.get('message')
+  }
+  console.log(submission)
+
+  // send post request
+
+  if (submission.message.length < 10) {
+    return {error: 'message must be more than 10'}
+  }
+
+
+  //redirect the user
+  return redirect('/')
 }
